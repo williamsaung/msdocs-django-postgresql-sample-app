d2601aff0750274612395ce40f52a86256667c2b
Williams <39475590+williamsaung@users.noreply.github.com>
2023-03-20 19:47:26 +0700

    changed db settings

--- azureproject/production.py
+++ azureproject/production.py
@@ -27,14 +27,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+# conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+# conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cbsconnect23',
+        'USER': 'cbscon23e4g0YEyxSqC@orion-mysql-accchula',
+        'PASSWORD': 'jV881kM4uW8xMoqQANLr',
+        'HOST': 'orion-mysql-accchula.mysql.database.azure.com',
     }
 }
